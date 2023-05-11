256dda825febd235640accd6ce4b820192a435d2
lrnz13 <131482390+lrnz13@users.noreply.github.com>
2023-05-11 12:24:18 -0600

    Delete CONSULTA.py

--- CONSULTA.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from sqlite3 import connect
-import streamlit as st
-conection = connect("InfoDatabase.db")
-consulta = pd.read_sql('SELECT * FROM ProjectsDB', conection)
-conection.close()
-st.write(consulta)
-
